e1eab4d5a92af4e21484fb65659f9cfacb426cba
Khang Nguyen <nguyenkhang10a@gmail.com>
2023-07-28 14:02:00 +0930

    Commit Week 2 Task B

---
+++ KhangNguyen_S331735/Week_2/Dotnet Application/Week2_TaskB/Models/Domain/Language.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace cinema_admin.Models.Domain
+{
+	public class Language
+	{
+        public int Id { get; set; }
+        [Required]
+        public string languageName { get; set; }
+
+    
+    }
+}
+
---
+++ KhangNguyen_S331735/Week_2/Dotnet Application/Week2_TaskB/Models/Domain/Movie.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cinema_admin.Models.Domain
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        public string movieName { get; set; }
+
+        [Required]
+       
+
+      
+        public int CategoryId { get; set; }
+        [Required]
+        public int LanguageId { get; set; }
+        [Required]
+        public int DirectorId { get; set; }
+
+        [NotMapped]
+        public string? directorName { get; set; }
+        [NotMapped]
+        public string? languageName { get; set; }
+        [NotMapped]
+        public string? categoryType { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem>? AuthorList { get; set; }
+        [NotMapped]
+        public List<SelectListItem>? PublisherList { get; set; }
+        [NotMapped]
+        public List<SelectListItem>? GenreList { get; set; }
+
+    }
+}
