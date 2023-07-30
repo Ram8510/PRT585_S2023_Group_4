2ddce790d47340f37e9e1c47cd86d50c11c80957
Ram8510 <112536391+Ram8510@users.noreply.github.com>
2023-07-30 20:52:36 +0930

    Created a Player Profile form

---
+++ Ram_S360556/Week2/Task A/src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.scss']
+})
+export class EmpAddEditComponent {
+  empForm: FormGroup;
+  position: string[] = [
+    'Forward',
+    'Midfielder',
+    'Defender',
+    'GoalKeeper'
+  ];
+  
+  constructor(private _fb: FormBuilder) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      country: '',
+      email: '',
+      dob: '',
+      position: '',
+    })
+  }
+
+onFormSubmit() {
+  if (this.empForm.valid) {
+    console.log(this.empForm.value);
+  }
+  }
+}
