eaf5cbb4c529ced7fd42bd33ec11756ae86ed3f5
Ram8510 <ramrmd8510@gmail.com>
2023-09-05 08:15:28 +0930

    First WebAPI-Units Added

--- Group_4_Software_Project/2DataAccessLayer/Context/Models/Unit.cs
+++ Group_4_Software_Project/2DataAccessLayer/Context/Models/Unit.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1CommonInfrastructure.Models
+namespace _2DataAccessLayer.Context.Models
 {
-    public class UnitModel
+    public class Unit
     {
         public int UnitId { get; set; } // int
         public string UnitCode { get; set; } // nvarchar(400)
         public string UnitName { get; set; } // nvarchar(400)
     }
-
+    
 }
