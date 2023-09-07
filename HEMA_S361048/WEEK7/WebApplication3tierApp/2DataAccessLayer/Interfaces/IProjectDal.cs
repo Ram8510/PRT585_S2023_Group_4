378ecaf356a8d292b22b4d542755a89d442afc2e
hema <hemalatha2v@gmail.com>
2023-09-07 11:50:30 +0930

    Add Web API Folder

---
+++ HEMA_S361048/WEEK7/WebApplication3tierApp/2DataAccessLayer/Interfaces/IProjectDal.cs
@@ -0,0 +1,21 @@
+﻿using _1CommonInfrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DataAccessLayer.Interfaces
+{
+    public interface IProjectDal
+    {
+        // Getters
+        ProjectModel? GetById(int ProjectId);
+        List<ProjectModel> GetAll();
+
+        // Actions
+        int CreateProject(ProjectModel Project);
+        void UpdateProject(ProjectModel Project);
+        void DeleteProject(int ProjectId);
+    }
+}
---
+++ HEMA_S361048/WEEK7/WebApplication3tierApp/2DataAccessLayer/Maps/ProjectMapExtensions.cs
@@ -0,0 +1,40 @@
+﻿using _1CommonInfrastructure.Models;
+using _2DataAccessLayer.Context.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DataAccessLayer.Maps
+{
+    public static class ProjectMapExtensions
+    {
+        public static ProjectModel ToProjectModel(this Project src)
+        {
+            var dst = new ProjectModel();
+
+            dst.ProjectId = src.ProjectId;
+            dst.ProjectCode = src.ProjectCode;
+            dst.ProjectName = src.ProjectName;
+
+            return dst;
+        }
+
+        public static Project ToProject(this ProjectModel src, Project dst = null)
+        {
+            if (dst == null)
+            {
+                dst = new Project();
+            }
+
+            dst.ProjectId = src.ProjectId;
+            dst.ProjectCode = src.ProjectCode;
+
+            dst.ProjectName = src.ProjectName;
+
+            return dst;
+        }
+    }
+}
