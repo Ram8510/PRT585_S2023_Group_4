d38f2a6694d9fee5b0d6585873cf2670170aac5f
Harashdeep Singh <s360797@students.cdu.edu.au>
2023-09-11 01:38:11 +0930

    Working with Angular app

--- Harashdeep_S360797/Week_4/movie-management/src/app/app.module.ts
+++ Harashdeep_S360797/Week_4/movie-management/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MovieCreateEditComponent } from './movie-create-edit/movie-create-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCreateEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
